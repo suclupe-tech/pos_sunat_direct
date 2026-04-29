0b088941a2df14188dfdcc22916ff1850e111bc7
C Suclupe <c.suclupe11@gmail.com>
2026-04-29 15:01:13 -0500

    mm

--- models/sunat_summary_batch.py
+++ models/sunat_summary_batch.py
@@ -113,29 +113,31 @@ class SunatSummaryBatch(models.Model):
                 )
 
                 if not match and "ya fue presentado anteriormente" in response:
-                    m = re.search(r"valor:\s*&quot;([^&]+)&quot;", response)
+                    m = re.search(r"ticket:\s*([^,]+)", response)
 
                     if not m:
                         m = re.search(r"valor:\s*\"([^\"]+)\"", response)
 
-                if not m:
-                    m = re.search(r"valor:\s*'([^']+)'", response)
+                    if not m:
+                        m = re.search(r"valor:\s*'([^']+)'", response)
 
-                if m:
-                    match = m
+                    if m:
+                        match = m
 
-                if not match:
-                    batch.write(
-                        {
-                            "name": rc_id,
-                            "state": "error",
-                            "xml_file": base64.b64encode(signed_xml.encode("utf-8")),
-                            "xml_filename": xml_name,
-                            "zip_file": zip_binary,
-                            "zip_filename": zip_name,
-                            "response_message": f"HTTP {status_code}\n{response[:3000]}",
-                        }
-                    )
+                    if not match:
+                        batch.write(
+                            {
+                                "name": rc_id,
+                                "state": "error",
+                                "xml_file": base64.b64encode(
+                                    signed_xml.encode("utf-8")
+                                ),
+                                "xml_filename": xml_name,
+                                "zip_file": zip_binary,
+                                "zip_filename": zip_name,
+                                "response_message": f"HTTP {status_code}\n{response[:3000]}",
+                            }
+                        )
                     continue
 
                 ticket = match.group(1)
