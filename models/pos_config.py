eac1af6e1166dccbfe983e22fbf198fe1260663f
C Suclupe <c.suclupe11@gmail.com>
2026-04-29 08:46:15 -0500

    mm

--- models/pos_config.py
+++ models/pos_config.py
@@ -17,11 +17,9 @@ class PosConfig(models.Model):
     sunat_serie_factura = fields.Char(string="Serie Factura SUNAT")
     sunat_serie_boleta = fields.Char(string="Serie Boleta SUNAT")
     sunat_user = fields.Char(string="Usuario SUNAT")
-    sunat_password = fields.Char(string="Clave SUNAT", groups="base.group_system")
+    sunat_password = fields.Char(string="Clave SUNAT")
     sunat_certificate_path = fields.Char(string="Ruta Certificado")
-    sunat_certificate_password = fields.Char(
-        string="Clave Certificado", groups="base.group_system"
-    )
+    sunat_certificate_password = fields.Char(string="Clave Certificado")
 
     class PosConfig(models.Model):
         _inherit = "pos.config"
